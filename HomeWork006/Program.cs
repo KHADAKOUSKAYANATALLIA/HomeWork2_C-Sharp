7da3cc4aaad3d25a87f8d7874c282862847f611c
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-22 20:44:41 +0300

    HomeWork006

---
+++ HomeWork006/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет. */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int result = 1;
+
+if (number >= 100)
+{                  
+    int thirdDigit = number;  // number / 10; 
+    result = thirdDigit % 10;
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+} 
+
+    
+Console.WriteLine("Третья цифра заданного числа: " + result);
+
