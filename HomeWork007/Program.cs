a538cedeb28e37c74938dd358a54c50ea1c303f5
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-22 15:47:21 +0300

    HomeWork007

---
+++ HomeWork007/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int NumberDay = Convert.ToInt32( Console.ReadLine() );
+
+switch (NumberDay){
+    case 1:        
+    Console.Write("Понедельник"); 
+    break;
+    case 2:        
+    Console.Write("Вторник"); 
+    break;
+    case 3:        
+    Console.Write("Среда"); 
+    break;
+    case 4:        
+    Console.Write("Четверг"); 
+    break;
+    case 5:        
+    Console.Write("Пятница"); 
+    break;
+    case 6:        
+    Console.Write("Суббота"); 
+    break;
+    case 7:        
+    Console.Write("Воскресенье"); 
+    break;
+    default:
+    Console.Write("В неделе 7 дней, введите цифру от 1 до 7: "); 
+    break;
+}
+
